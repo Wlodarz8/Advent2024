72f8890712a2797fc407e457f5541160b5415946
Michal Wlodarczak <michalek.wlodarczak@gmail.com>
2024-12-01 16:24:15 +0100

    Dzień pierwszy advent of code

---
+++ Day1/Day1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include<algorithm>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    ifstream file("input1.txt");
+
+    vector<int> left;
+    vector<int> right;
+
+    int first, second;
+    while (file >> first >> second) {
+        left.push_back(first);
+        right.push_back(second);
+    }
+
+    file.close();
+
+    // Part 1
+    sort(left.begin(), left.end());
+    sort(right.begin(), right.end());
+    int suma = 0;
+    for (int i = 0; i < left.size(); i++){
+        suma += abs(left[i] - right[i]);
+    }
+
+    cout<<suma<<endl;
+
+    // Part 2
+    int suma2 = 0;
+    for (int i = 0; i < left.size(); i++){
+        int lewy = left[i];
+        int counter = 0;
+        for (int j = 0; j < right.size(); j++){
+            if (lewy == right[j]){
+                counter++;
+            }
+        }
+        suma2 += lewy * counter;
+    }
+    cout<<suma2;
+    return 0;
+}
